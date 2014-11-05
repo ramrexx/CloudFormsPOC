f98f97d512237a6e1c14ff0a60358bbba893147d
ramrexx <kmorey@redhat.com>
2014-11-05 13:34:13 -0800

    Initial Domain Import

--- Automate/CloudFormsPOC/Service/Provisioning/StateMachines/ServiceProvisionRequestQuotaVerification.class/__methods__/service_request_rejected.rb
+++
@@ -1,8 +0,0 @@
-#
-# service_request_rejected.rb
-#
-# Description: This method runs when the service request quota validation has failed
-#
-
-$evm.log(:info, "Request denied because of Quota")
-$evm.root["miq_request"].deny("admin", "Quota Exceeded")
--- Automate/CloudFormsPOC/System/Policy.class/__methods__/vmreconfigurerequest_auto_approve.rb
+++
@@ -1,6 +0,0 @@
-# VmReconfigureRequest_Auto_Approve.rb
-# Kevin Morey
-# Description: This method auto-approves the vm reconfiguration request
-# Auto-Approve request
-$evm.log("info", "AUTO-APPROVING")
-$evm.root["miq_request"].approve("admin", "Auto-Approved")
