bea6338dcef49646e154b0c0cdc0f1184ec4ef96
root <root@host125.rdu.salab.redhat.com>
2015-03-13 20:02:25 -0400

    Corrected issue with vmware mark as a virtual machine and list resource pools

--- Automate/CloudFormsPOC/Integration/VMware/DynamicDropDowns.class/__methods__/list_resource_pools.rb
+++ Automate/CloudFormsPOC/Integration/VMware/DynamicDropDowns.class/__methods__/list_resource_pools.rb
@@ -1,27 +1,61 @@
 # List_Resource_Pools.rb
 #
+# Author: Kevin Morey <kmorey@redhat.com>
+# License: GPL v3
+#
 # Description: List the resource pools associated with a provider
 #
+begin
+  def log(level, msg, update_message=false)
+    $evm.log(level, "#{msg}")
+  end
 
-# Get vm object from root
-vm = $evm.root['vm']
-raise "Missing $evm.root['vm'] object" if vm.nil?
+  def dump_root()
+    $evm.log(:info, "Begin $evm.root.attributes")
+    $evm.root.attributes.sort.each { |k, v| log(:info, "\t Attribute: #{k} = #{v}")}
+    $evm.log(:info, "End $evm.root.attributes")
+    $evm.log(:info, "")
+  end
 
-provider = vm.ext_management_system
-$evm.log(:info, "Detected Provider: #{provider.name}")
+  ###############
+  # Start Method
+  ###############
+  log(:info, "CloudForms Automate Method Started", true)
+  dump_root()
 
-pools_hash = {}
-#pools_hash = {'<choose>' => nil}
+  # Get vm object from root
+  vm = $evm.root['vm']
+  raise "Missing $evm.root['vm'] object" if vm.nil?
 
-provider.resource_pools.each do |pool|
-  #next unless template.tagged_with?('prov_scope', 'all')
-  #next unless template.vendor.downcase == 'vmware'
-  if vm.resource_pool.ems_ref == pool.ems_ref
-    pools_hash[pool[:ems_ref]] = "<current> #{pool[:name]}"
-  else
-    pools_hash[pool[:ems_ref]] = pool[:name]
-  end  
+  provider = vm.ext_management_system
+  $evm.log(:info, "Detected Provider: #{provider.name}")
+
+  pools_hash = {}
+
+  provider.resource_pools.each do |pool|
+    log(:info, "Looking at resource_pool: #{pool.name} id: #{pool.id} ems_ref: #{pool.ems_ref}")
+    #next unless template.tagged_with?('prov_scope', 'all')
+    #next unless template.vendor.downcase == 'vmware'
+    if vm.resource_pool && vm.resource_pool.ems_ref == pool.ems_ref
+      pools_hash[pool[:ems_ref]] = "<current> #{pool[:name]}"
+    else
+      pools_hash[pool[:ems_ref]] = pool[:name]
+    end
+  end
+  pools_hash[nil] = '< Choose a pool >'
+
+  $evm.object['values'] = pools_hash
+  $evm.log(:info, "Dialog Values: #{$evm.object['values'].inspect}")
+
+  ###############
+  # Exit Method
+  ###############
+  log(:info, "CloudForms Automate Method Ended", true)
+  exit MIQ_OK
+
+  # Set Ruby rescue behavior
+rescue => err
+  log(:error, "[#{err}]\n#{err.backtrace.join("\n")}")
+  @task.finished("#{err}") if @task
+  exit MIQ_ABORT
 end
-
-$evm.object['values'] = pools_hash
-$evm.log(:info, "Dialog Values: #{$evm.object['values'].inspect}")
--- Automate/CloudFormsPOC/Integration/VMware/vCenter.class/__methods__/markvmasvirtualmachine.rb
+++ Automate/CloudFormsPOC/Integration/VMware/vCenter.class/__methods__/markvmasvirtualmachine.rb
@@ -1,63 +1,93 @@
 # MarkVMAsVirtualMachine.rb
 #
+# Author: Kevin Morey <kmorey@redhat.com>
+# License: GPL v3
+#
 # Description: This method marks a VMware template as a VM
 #
-
-require 'savon'
-
-def login(client, username, password)
-  result = client.call(:login) do
-    message( :_this => "SessionManager", :userName => username, :password => password )
+begin
+  def log(level, msg, update_message=false)
+    $evm.log(level, "#{msg}")
   end
-  client.globals.headers( { "Cookie" => result.http.headers["Set-Cookie"] } )
-end
 
-def logout(client)
-  begin
-    client.call(:logout) do
-      message(:_this => "SessionManager")
+  def dump_root()
+    $evm.log(:info, "Begin $evm.root.attributes")
+    $evm.root.attributes.sort.each { |k, v| log(:info, "\t Attribute: #{k} = #{v}")}
+    $evm.log(:info, "End $evm.root.attributes")
+    $evm.log(:info, "")
+  end
+
+  require 'savon'
+
+  def login(client, username, password)
+    result = client.call(:login) do
+      message( :_this => "SessionManager", :userName => username, :password => password )
     end
-  rescue => logouterr
-    $evm.log(:error, "Error logging out #{logouterr.class} #{logouterr}")
+    client.globals.headers( { "Cookie" => result.http.headers["Set-Cookie"] } )
   end
+
+  def logout(client)
+    begin
+      client.call(:logout) do
+        message(:_this => "SessionManager")
+      end
+    rescue => logouterr
+      log(:error, "Error logging out #{logouterr.class} #{logouterr}")
+    end
+  end
+
+  ###############
+  # Start Method
+  ###############
+  log(:info, "CloudForms Automate Method Started", true)
+  dump_root()
+
+  # Get vm object from root
+  vm = $evm.root['vm']
+  raise "VM object not found" if vm.nil?
+
+  # This method only works with VMware VMs currently
+  raise "Invalid vendor: #{vm.vendor}" unless vm.vendor.downcase == 'vmware'
+
+  log(:info, "Detected VM: #{vm.name} vendor: #{vm.vendor} provider: #{vm.ext_management_system.name} ems_ref: #{vm.ems_ref}")
+
+  # get resource_pool.ems_ref from root
+  resource_pool = $evm.root['dialog_resource_pool']
+
+  # get servername and credentials from vm.ext_management_system
+  servername = vm.ext_management_system.ipaddress
+  username = vm.ext_management_system.authentication_userid
+  password = vm.ext_management_system.authentication_password
+
+  client = Savon.client(
+    :wsdl => "https://#{servername}/sdk/vim.wsdl",
+    :endpoint => "https://#{servername}/sdk/",
+    :ssl_verify_mode => :none,
+    :ssl_version => :TLSv1,
+    :raise_errors => false,
+    :log_level => :info,
+    :log => false
+  )
+  #client.operations.sort.each { |operation| $evm.log(:info, "Savon Operation: #{operation}") }
+  # login and set cookie
+  login(client, username, password)
+
+  mark_as_virtual_machine_result = client.call(:mark_as_virtual_machine) do
+    message( { '_this' => vm.ems_ref, :attributes! => { 'type' => "VirtualMachine"},
+               'pool' => resource_pool } ).to_hash
+  end
+  log(:info, "mark_as_virtual_machine_result success?: #{mark_as_virtual_machine_result.success?}")
+
+  logout(client)
+
+  ###############
+  # Exit Method
+  ###############
+  log(:info, "CloudForms Automate Method Ended", true)
+  exit MIQ_OK
+
+  # Set Ruby rescue behavior
+rescue => err
+  log(:error, "[#{err}]\n#{err.backtrace.join("\n")}")
+  exit MIQ_ABORT
 end
-
-# Get vm object from root
-vm = $evm.root['vm']
-raise "VM object not found" if vm.nil?
-
-# This method only works with VMware VMs currently
-raise "Invalid vendor:<#{vm.vendor}>" unless vm.vendor.downcase == 'vmware'
-
-$evm.log(:info,"Detected VM: #{vm.name} vendor: #{vm.vendor} provider: #{vm.ext_management_system.name} ems_ref: #{vm.ems_ref}")
-
-# get resource_pool.ems_ref from root
-  resource_pool = $evm.root['dialog_resource_pool_ems_ref']
-
-# get servername and credentials from vm.ext_management_system
-servername = vm.ext_management_system.ipaddress
-username = vm.ext_management_system.authentication_userid
-password = vm.ext_management_system.authentication_password
-
-client = Savon.client(
-  :wsdl => "https://#{servername}/sdk/vim.wsdl",
-  :endpoint => "https://#{servername}/sdk/",
-  :ssl_verify_mode => :none,
-  :ssl_version => :TLSv1,
-  :raise_errors => false,
-  :log_level => :info,
-  :log => false
-)
-#client.operations.sort.each { |operation| $evm.log(:info, "Savon Operation: #{operation}") }
-
-# login and set cookie
-login(client, username, password)
-
-mark_as_virtual_machine_result = client.call(:mark_as_virtual_machine) do
-  message( { '_this' => vm.ems_ref, :attributes! => { 'type' => "VirtualMachine"},
-             'pool' => resource_pool } ).to_hash
-end
-#$evm.log(:warn, "mark_as_virtual_machine_result: #{mark_as_virtual_machine_result.inspect}")
-$evm.log(:info, "mark_as_virtual_machine_result success?: #{mark_as_virtual_machine_result.success?}")
-
-logout(client)
