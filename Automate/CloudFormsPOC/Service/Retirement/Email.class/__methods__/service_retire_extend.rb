0e91f8888d27692a17b23b8f996ccdec10f6b388
Ramrexx <kmorey@redhat.com>
2015-03-24 15:11:21 -0400

    Updated service_retire_extend to support retirement date synchronization amongst a service and all child vms

--- Automate/CloudFormsPOC/Service/Retirement/Email.class/__methods__/service_retire_extend.rb
+++ Automate/CloudFormsPOC/Service/Retirement/Email.class/__methods__/service_retire_extend.rb
@@ -3,8 +3,8 @@
 # Author: Kevin Morey <kmorey@redhat.com>
 # License: GPL v3
 #
-# Description: This method is used to add x days to retirement date when target Service has a retires_on value and is not
-# already retired, then loop through all child vms and synchronize the retires_on date
+# Description: This method will add a retirement extension to the the oldest retires_on and retirement_warn found on a service and all child vms.
+#  For example, service retires_on = '2015-11-11', vm1 retires_on = '2015-06-16'. The service and all child vms will retire on '2015-06-16' (the oldest).
 #
 begin
   def log(level, msg, update_message=false)
@@ -25,56 +25,70 @@ begin
   dump_root()
 
   # Number of days to extend retirement
-  service_retire_extend_days = nil || $evm.root['dialog_retire_extend_days'] || $evm.object['service_retire_extend_days']
-  service_retire_extend_days = service_retire_extend_days.to_i
-  log(:info, "Number of days to extend: #{service_retire_extend_days}")
+  retire_extend_days = nil || $evm.root['dialog_retire_extend_days'] || $evm.object['service_retire_extend_days']
+  retire_extend_days = retire_extend_days.to_i
+  log(:info, "Number of days to extend: #{retire_extend_days}")
 
   service = $evm.root['service']
   service.attributes.each {|k, v| log(:info, "Service: #{service.name} updated {#{k} => #{v.inspect}}") if k.include?('retire') }
+  retirement_dates = []
+  retirement_warnings = []
 
-  if service.retires_on
-    unless service_retire_extend_days.zero?
-      log(:info, "Extending retirement #{service_retire_extend_days} days for Service: #{service.name}")
-      # Set new retirement date here
-      service.retires_on += service_retire_extend_days
-      service.attributes.each {|k, v| log(:info, "Service: #{service.name} updated {#{k} => #{v.inspect}}") if k.include?('retire') }
-      vm_body = ''
-      service.vms.each do |vm|
-        vm.retires_on = service.retires_on
-        vm.retirement_warn = service.retirement_warn
-        vm.attributes.each {|k, v| log(:info, "VM: #{vm.name} updated {#{k} => #{v.inspect}}") if k.include?('retire') }
-        vm_body += "<br><br>The retirement date for VM: #{vm.name} has been set to: #{vm.retires_on}."
-      end
-      # Get Service Owner Name and Email
-      owner_id = service.evm_owner_id
-      owner = $evm.vmdb('user', owner_id) unless owner_id.nil?
+  log(:info, "Service: #{service.name} - retires_on: #{service.retires_on} retirement_warn: #{service.retirement_warn}")
+  retirement_dates << service.retires_on.to_s
+  retirement_warnings << service.retirement_warn
 
-      # to_email_address from owner.email then from model if nil
-      to = owner.email if owner.email
-      to ||= $evm.object['to_email_address']
+  service.vms.each do |vm|
+    log(:info, "VM: #{vm.name} - retires_on: #{vm.retires_on} retirement_warn: #{vm.retirement_warn}")
+    retirement_dates << vm.retires_on.to_s
+    retirement_warnings << vm.retirement_warn
+  end
 
-      # Get from_email_address from model unless specified below
-      from = nil || $evm.object['from_email_address']
-
-      # Get signature from model unless specified below
-      signature = nil || $evm.object['signature']
-
-      # email subject
-      subject = "Service Retirement Extended for #{service.name}"
-
-      # Build email body
-      body = "Hello, "
-      body += "<br><br>The retirement date for service: #{service.name} has been set to: #{service.retires_on}."
-      body += vm_body
-      body += "<br><br> Thank you,"
-      body += "<br> #{signature}"
-
-      # Send email
-      log(:info, "Sending email to #{to} from #{from} subject: #{subject}")
-      $evm.execute('send_email', to, from, subject, body)
+  oldest_date = retirement_dates.sort!.last
+  log(:info, "Retirement date: #{oldest_date} found out of retirement_dates: #{retirement_dates}")
+  largest_warning = retirement_warnings.sort!.last
+  log(:info, "Retirement warning: #{largest_warning} found out of retirement_warnings: #{retirement_warnings}")
+  
+  unless oldest_date.blank?
+  new_date = oldest_date.to_date + retire_extend_days
+  log(:info, "Adding extended retirement date: #{new_date.to_s}")
+    service.retires_on = new_date.to_s
+    service.retirement_warn = largest_warning
+    service.attributes.each {|k, v| log(:info, "Service: #{service.name} updated {#{k} => #{v.inspect}}") if k.include?('retire') }
+    vm_body = ''
+    service.vms.each do |vm|
+      vm.retires_on = new_date.to_s
+      vm.retirement_warn = largest_warning
+      vm.attributes.each {|k, v| log(:info, "VM: #{vm.name} updated {#{k} => #{v.inspect}}") if k.include?('retire') }
+      vm_body += "<br><br>The retirement date for VM: #{vm.name} has been set to: #{vm.retires_on} warning: #{vm.retirement_warn} days."
     end
-  else
-    log(:info, "Service: #{service.name} has no retirement date set...skipping")
+    # Get Service Owner Name and Email
+    owner_id = service.evm_owner_id
+    owner = $evm.vmdb('user', owner_id) unless owner_id.nil?
+
+    # to_email_address from owner.email then from model if nil
+    to = owner.email if owner.email
+    to ||= $evm.object['to_email_address']
+
+    # Get from_email_address from model unless specified below
+    from = nil || $evm.object['from_email_address']
+
+    # Get signature from model unless specified below
+    signature = nil || $evm.object['signature']
+
+    # email subject
+    subject = "Service Retirement Extended for #{service.name}"
+
+    # Build email body
+    body = "Hello, "
+    body += "<br><br>The retirement date for service: #{service.name} has been set to: #{service.retires_on} warning: #{service.retirement_warn} days."
+    body += vm_body
+    body += "<br><br> Thank you,"
+    body += "<br> #{signature}"
+
+    # Send email
+    log(:info, "Sending email to #{to} from #{from} subject: #{subject}")
+    $evm.execute('send_email', to, from, subject, body)
   end
 
   ###############
